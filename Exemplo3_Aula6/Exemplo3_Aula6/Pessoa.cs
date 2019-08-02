3ca3e9843f87019735b3feb5657178d93f651d43
Jefferson de Assis <38639576+jefferson-assis@users.noreply.github.com>
2019-08-01 21:36:56 -0300

    Exemplo de orientada a objetos

---
+++ Exemplo3_Aula6/Exemplo3_Aula6/Pessoa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo3_Aula6
+{
+    class Pessoa
+    {
+        //Atributos da classe
+        private string nome;
+        private int idade;
+        private double altura;
+        private string sexo;
+
+        //Construtor sem parâmetros
+        public Pessoa()
+        {
+            this.nome = null;
+            this.idade = 0;
+            this.altura = 0.0;
+            sexo = null;
+        }
+
+        //Construtor com parâmetros
+        public Pessoa(string nome, int idade, double altura, string sexo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+            sexo = sexo.ToString();
+        }
+
+        //Propriedade do campo nome para acesso ao seu conteúdo
+        public string Nome
+        {
+            get { return nome; } //retornar o valor armazenado no campo nome.
+            set { nome = value; } //Armazenar  o valor no campo.
+        }
+
+        public int Idade
+        {
+
+        }
+    }
+}
---
+++ Exemplo3_Aula6/Exemplo3_Aula6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo3_Aula6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
