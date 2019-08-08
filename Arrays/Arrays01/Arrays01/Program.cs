943b562c4646bdda24224f79104c09bd93bf7d04
jefferson-assis <jeffersondeassis13022000@gmail.com>
2019-08-08 19:34:37 -0300

    Adicionando Array

---
+++ Arrays/Arrays01/Arrays01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("<----Vetor---->\n ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            int[] valor = new int[50];
+            valor[0] = 25;
+            valor[25] = 25;
+
+            int x;
+            for (x=0;x<5;x++){
+                Console.Write("Digite um valor: ");
+                valor[x] = int.Parse(Console.ReadLine());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
