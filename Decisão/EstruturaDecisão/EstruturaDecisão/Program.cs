825e7a80753948e13662f714e916942b39f3e3ff
jefferson-assis <jeffersondeassis13022000@gmail.com>
2019-08-08 20:04:25 -0300

    ExeDecis

---
+++ Decisão/EstruturaDecisão/EstruturaDecisão/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDecisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            int ano, idade;
+            Console.Write("Digite seu Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Digite o ano de Nascimento: ");
+            ano = int.Parse(Console.ReadLine());
+            idade = 2019 - ano;
+
+            if (idade < 18)
+            {
+                Console.Write("Você é menor de Idade!");
+            }
+            else
+            {
+                Console.Write("Você é maior de Idade!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Decisão/ExercicioDecisão/ExercicioDecisão/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDecisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d;
+            Console.Write("Digite a Nota a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Digite a Nota b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Digite a Nota c: ");
+            c = double.Parse(Console.ReadLine());
+            d = (a + b + c) / 3;
+            if (a < 7) ;
+                {
+                
+            }
+        }
+    }
+}
